f43850216950a5b2a2bfb9237331cb090eaa0bb0
Anushka Verma <v.anushka786@gmail.com>
2020-02-03 23:37:30 +0530

    add posts model

---
+++ server/models/Post.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new mongoose.Schema({
+  // Reference to User Schema
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  // Content of the post
+  content: {
+    type: String,
+    required: true
+  },
+  // Authore's name
+  name: {
+    type: String
+  },
+  // Author's profile image
+  avatar: {
+    type: String
+  },
+  // #likes on a comment
+  likes: [
+    // array of objects
+    {
+      // Reference to User Schema
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  // Comments on the post
+  comments: [
+    // array of objects
+    {
+      // Reference to User Schema
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      // Content of the comment
+      content: {
+        type: String,
+        required: true
+      },
+      // Authore's name
+      name: {
+        type: String
+      },
+      // Author's profile image
+      avatar: {
+        type: String
+      },
+      // Date of comment
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  // Date of posts
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
