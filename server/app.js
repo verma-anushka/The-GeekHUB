cff6312a0c7eac049463241ba1a0394fcfa3a097
Anushka Verma <v.anushka786@gmail.com>
2020-02-21 18:16:13 +0530

    heroku deployment

--- server/app.js
+++ server/app.js
@@ -34,10 +34,10 @@ app.use("/api/posts", posts);
 // PRODUCTION MODE SETTINGS
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
   });
 }
 
