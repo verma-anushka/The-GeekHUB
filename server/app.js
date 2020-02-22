30664b9d47044f2a5dc03c1c54f9770d38224851
Anushka Verma <v.anushka786@gmail.com>
2020-02-22 19:28:22 +0530

    heroku deployment

--- server/app.js
+++ server/app.js
@@ -34,7 +34,8 @@ app.use("/api/posts", posts);
 // PRODUCTION MODE SETTINGS
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "../client/build/index.html"));
@@ -49,3 +50,7 @@ app.listen(app.get("port"), () => {
 
 // "start": "node app.js", // npm start
 // "server": "nodemon app.js" // npm run server
+// "client-install": "npm install --prefix client",
+// "client": "npm start --prefix client",
+
+// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
