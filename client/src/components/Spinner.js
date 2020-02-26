22a5986702ea198c7c4e601e66b8f90db9d1a8ca
Anushka Verma <v.anushka786@gmail.com>
2020-02-26 16:00:29 +0530

    add loading spinner

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "../assets/styles/components/Spinner.scss";
 
 export default function Spinner() {
   return (
-    <div className="sinner">
-      <h1>Spinner..........</h1>
+    // <div classNameName="spinner">
+    //   <h1>Spinner..........</h1>
+    // </div>
+
+    <div class="loader JS_on">
+      <span class="binary"></span>
+      <span class="binary"></span>
+      <span class="binary"></span>
+      <span class="binary"></span>
     </div>
   );
 }
